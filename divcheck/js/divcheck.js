4ee41f6c537a8beef35bc85c4b85377fcf80302f
kelicom <735579768@qq.com>
2016-01-28 11:27:05 +0800

    完善功能bug

--- divcheck/js/divcheck.js
+++ divcheck/js/divcheck.js
@@ -32,25 +32,31 @@ $(function() {
 			tongbu: function() {
 				var _t = this;
 				_t.s_dest.attr('value', _t.s_src.val());
-				if (_t.s_src.is(':checked')) {
+				if (_t.s_src.prop('checked')) {
 					_t.s_dest.addClass('kl-selected');
+				}else{
+					_t.s_dest.removeClass('kl-selected');
 				}
 
-
+				_t.s_dest.unbind( "click" );
+				_t.s_src.unbind( "change" );
+				_t.s_src.unbind( "click" );
 				_t.s_dest.click(function(event) {
 					var _tt = $(this);
 					if (_tt.hasClass('kl-selected')) {
 						_tt.removeClass('kl-selected');
-						_t.s_src.attr('checked', false);
+						_t.s_src.prop('checked')&&_t.s_src.prop('checked', false);
 					} else {
 						_tt.addClass('kl-selected');
-						_t.s_src.attr('checked', true);
+						_t.s_src.prop('checked')||_t.s_src.prop('checked', true);
 					}
-					_t.s_src.click();
 				});
 				this.s_src.change(function(event) {
 					_t.tongbu();
 				});
+/*				this.s_src.click(function(event) {
+					_t.tongbu();
+				});*/
 			}
 		};
 		window.hidecheckbox = {};
@@ -85,12 +91,12 @@ $(function() {
 						b.s_dest.addClass(arg[1]);
 					}
 				}
-				var na = 'id__' + b.s_src.attr('id');
-				(na == 'id__undefined') && (na = 'name__' + b.s_src.attr('name'));
+				var na = 'id__' + b.s_src.prop('id');
+				(na == 'id__undefined') && (na = 'name__' + b.s_src.prop('name'));
 				(na == 'name__undefined') && (na = index);
 				var id = 'hidecheckbox__' + na;
 				allcheckbox[id] = b;
-				b.s_dest.attr('id', id);
+				b.s_dest.prop('id', id);
 				b.id = id;
 				tem.push(b);
 			});
--- divcheck/js/divradio.js
+++ divcheck/js/divradio.js
@@ -24,7 +24,7 @@ $(function() {
 				}
 				this.s_dest = _s_src.prev();
 				_s_src.hide();
-				var _name = this.s_src.attr('name');
+				var _name = this.s_src.prop('name');
 				this.group = $('input[name="' + _name + '"][type="radio"]');
 				this.tongbu();
 			},
@@ -34,17 +34,20 @@ $(function() {
 			},
 			tongbu: function() {
 				var _t = this;
-				_t.s_dest.attr('value', _t.s_src.val());
-				if (_t.s_src.is(':checked')) {
+				_t.s_dest.prop('value', _t.s_src.val());
+				if (_t.s_src.prop('checked')) {
+					_t.group.prev().removeClass('kl-selected');
+					_t.group.prop('checked', false);
 					_t.s_dest.addClass('kl-selected');
+					_t.s_src.prop('checked', true);
 				}
+				_t.s_dest.unbind( "click" );
 				_t.s_dest.click(function(event) {
 					var _tt = $(this);
 					_t.group.prev().removeClass('kl-selected');
-					_t.group.attr('checked', false);
+					_t.group.prop('checked', false);
 					_tt.addClass('kl-selected');
-					_t.s_src.attr('checked', true);
-					_t.s_src.click();
+					_t.s_src.prop('checked', true);
 				});
 				this.s_src.change(function(event) {
 					_t.tongbu();
@@ -80,12 +83,12 @@ $(function() {
 						b.s_dest.addClass(arg[1]);
 					}
 				}
-				var na = 'id__' + b.s_src.attr('id');
-				(na == 'id__undefined') && (na = 'name__' + b.s_src.attr('name'));
+				var na = 'id__' + b.s_src.prop('id');
+				(na == 'id__undefined') && (na = 'name__' + b.s_src.prop('name'));
 				(na == 'name__undefined') && (na = index);
 				var id = 'hideradio__' + na+'__'+index;
 				allradio[id] = b;
-				b.s_dest.attr('id', id);
+				b.s_dest.prop('id', id);
 				b.id = id;
 				tem.push(b);
 			});
